e97a89d680b36a50c3a9efe8e3ccd78974039a29
Blues Park <blues.park@kakaocorp.com>
2022-12-26 14:38:40 +0900

    8080 포트를 사용한다.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8000);
+        Server server = new Server(8080);
     }
 }
